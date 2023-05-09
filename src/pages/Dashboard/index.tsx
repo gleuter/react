f584234c41a77b5c62edceaaf08b2dde5c9ec21d
Gleuter Pereira Barbosa <gpereirb@emeal.nttdata.com>
2023-05-08 21:33:07 -0300

    Aplicaçao final

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React from "react"
 import { FiChevronRight } from 'react-icons/fi'
+import {Link} from 'react-router-dom'
 
 import { api } from '../../services/api'
 import { Title, Form, Repos, Error } from './style'
 import logo from '../../assests/logo.svg'
+
 
 import { Repo } from "../Repo";
 
@@ -31,7 +33,7 @@ export const Dashbaord: React.FC = () => {
 
     const [newRepo, setNewRepo] = React.useState('')
     const [inputError, setInputError] = React.useState('')
-
+    const formEl = React.useRef<HTMLFormElement | null>(null);
 
     React.useEffect(() => {
         localStorage.setItem('@GitCollection:repositories', JSON.stringify(repos));
@@ -50,13 +52,19 @@ export const Dashbaord: React.FC = () => {
             setInputError('Informe o user name /repositório')
             return
         }
-
-        const response = await api.get<GithubRepository>(`repos/${newRepo}`)
-
-        const repository = response.data
-
-        setRepos([...repos, repository])
-        setNewRepo('')
+        try {
+            const response = await api.get<GithubRepository>(`repos/${newRepo}`);
+      
+            const repository = response.data;
+      
+            setRepos([...repos, repository]);
+            formEl.current?.reset();
+            setNewRepo('');
+            setInputError('');
+          } catch {
+            setInputError('Repositorio nao encontrado no Github');
+          }
+        
     }
 
 
@@ -65,7 +73,7 @@ export const Dashbaord: React.FC = () => {
             <img src={logo} alt="GitCollection" />
             <Title>Catalogo de repositorios do Github</Title>
 
-            <Form hasError={Boolean(inputError)} onSubmit={handleAddRepo}>
+            <Form ref={formEl}  hasError={Boolean(inputError)} onSubmit={handleAddRepo}>
                 <input placeholder="username/repository_name" onChange={handeInputChange} />
                 <button type="submit">Buscar</button>
             </Form>
@@ -74,7 +82,7 @@ export const Dashbaord: React.FC = () => {
 
             <Repos>
                 {repos.map(repository => (
-                    <a href={`/repositorie/${repository.full_name}`} 
+                    <Link to={`/repositories/${repository.full_name}`} 
                     key={repository.full_name}>
                         <img
                             src={repository.owner.avatar_url}
@@ -85,7 +93,7 @@ export const Dashbaord: React.FC = () => {
                             <p>{repository.description}</p>
                         </div>
                         <FiChevronRight size={20} />
-                    </a>
+                    </Link>
                 ))}
             </Repos>
         </>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom'
+import {Switch, Route} from 'react-router-dom';
 import { Dashbaord } from '../pages/Dashboard';
 import { Repo } from '../pages/Repo';
 
@@ -7,10 +7,9 @@ import { Repo } from '../pages/Repo';
 export const Routes: React.FC = () => {
   return (
     <Switch>
-      <Route  component={Dashbaord} path="/" exact />
-      <Route  component={Repo} path="/repositories/:repository"  />
+      <Route component={Dashbaord} path="/" exact />
+      <Route component={Repo} path="/repositories/:repository+"/>
     </Switch>
   );
 }
-
 
